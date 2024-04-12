b0ea560dfc9984f8716b99255d9c37c9e48e50c2
Thinh <thinhmvo101>
2024-04-12 18:14:30 -0400

    set up sample caroseul

--- moviedb/app/page.js
+++ moviedb/app/page.js
@@ -1,11 +1,12 @@
 import CardExample from "./components/CardExample";
+import ControlledCarousel from "./components/Carousel";
 import NavigationBar from "./components/NavigationBar";
 
 export default function Home() {
   return (
     <>
     <NavigationBar/>
-    <div class="container">
+    <div class="container mt-2">
     
       <CardExample class="mt-5"/>
      
